b00b4455721c71a1c7ff3e47a0d1b5fe4b5a9ce9
Anton Petrov <optsing@outlook.com>
2020-03-05 01:24:22 +0300

    Convert to umd library

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: {
-        api: path.join(__dirname, 'src', 'api.ts'),
-    },
+    entry: path.join(__dirname, 'src', 'api.ts'),
     output: {
         path: path.join(__dirname, 'lib'),
-        filename: '[name].js',
-        chunkFilename: '[name].chunk.js',
-        publicPath: '/'
+        filename: 'api.js',
+        library: 'scada-plugin-api',
+        libraryTarget: 'umd',
     },
     module: {
         rules: [
